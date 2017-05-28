d7b649856d82dcf74b7ed7dbfe9748ae488765bc
TMats <t.matsu.changmin@gmail.com>
2017-05-29 07:31:01 +0900

    add clustering logic by tf-idf cosine similarity

--- clustering/models.py
+++ clustering/models.py
@@ -5,6 +5,8 @@ from gensim.models.doc2vec import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 import numpy as np
 from scipy.sparse.csgraph import connected_components
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 # Create your models here.
@@ -35,7 +37,6 @@ class EnArticle(models.Model):
 class ArticleCluster(models.Model):
     article_id = models.IntegerField(primary_key=True)
     cluster_id = models.IntegerField()
-    # clustered_at = models.DateTimeField()
     created_at = models.DateTimeField(default=now())
     updated_at = models.DateTimeField(default=now())
 
@@ -43,7 +44,6 @@ class ArticleCluster(models.Model):
         managed = False
         db_table = 'article_clusters'
         # unique_together=('article_id', 'clustered_at')
-
 
     @classmethod
     def get_cluster_article(cls, cluster_id):
@@ -56,7 +56,7 @@ def tokenize_articles():
     return article_tokens, article_ids
 
 
-def cluster_by_doc2vec(threshold=0.75):
+def cluster_by_doc2vec(threshold=0.7):
     article_tokens, article_ids = tokenize_articles()
 
     sentences = [TaggedDocument(words=article_token, tags=[article_id]) for (article_token, article_id) in zip(article_tokens, article_ids)]
@@ -73,8 +73,26 @@ def cluster_by_doc2vec(threshold=0.75):
     cluster_ids = list(cluster_ids)
 
     # save cluster
-    # clustered_at = now()
     for (article_id, cluster_id) in zip(article_ids, cluster_ids):
-        # article_cluster = ArticleCluster(article_id=article_id, cluster_id=cluster_id, clustered_at=clustered_at)
+        article_cluster = ArticleCluster(article_id=article_id, cluster_id=cluster_id)
+        article_cluster.save()
+
+
+def cluster_by_tfidf(threshold=0.5):
+    article_ids = EnArticle.get_article_ids()
+    contents = [EnArticle.get_article(article_id).content for article_id in article_ids]
+    tfidf_vectorizer = TfidfVectorizer(tokenizer=tokenize, lowercase=False, stop_words=None)
+    similarity_matrix = np.array(cosine_similarity(tfidf_vectorizer.fit_transform(contents)))
+
+    matrix = np.eye(len(article_ids))
+    for i, row in enumerate(similarity_matrix):
+        row[i] = 0
+        if np.max(row) >= threshold:
+            matrix[i, np.argmax(row)] = 1
+    cluster_num, cluster_ids = connected_components(matrix)
+    cluster_ids = list(cluster_ids)
+
+    # save cluster
+    for (article_id, cluster_id) in zip(article_ids, cluster_ids):
         article_cluster = ArticleCluster(article_id=article_id, cluster_id=cluster_id)
         article_cluster.save()
